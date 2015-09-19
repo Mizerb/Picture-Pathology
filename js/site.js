c55907baa77659168038aee7996059dfef98d169
minstz <minstz@rpi.edu>
2015-09-19 14:31:14 -0400

    Basic Frontend setup
    Initialization

---
+++ js/site.js
@@ -0,0 +1,11 @@
+var myInput = document.getElementById('myFileInput');
+
+function sendPic() {
+    var file = myInput.files[0];
+
+    // Send file here either by adding it to a `FormData` object 
+    // and sending that via XHR, or by simply passing the file into 
+    // the `send` method of an XHR instance.
+}
+
+myInput.addEventListener('change', sendPic, false);
